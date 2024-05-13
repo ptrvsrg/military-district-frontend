9b49c4c9d9827baea1e97f007e018a4cbdd8f407
ptrvsrg <s.petrov1@g.nsu.ru>
2024-05-13 19:41:22 +0700

    Добавлена точка старта

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,31 @@
-import {StrictMode} from 'react'
-import {createRoot} from 'react-dom/client'
+import { ApolloProvider } from '@apollo/client'
+import { ThemeProvider } from '@mui/material'
+import { LocalizationProvider } from '@mui/x-date-pickers'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { ReactKeycloakProvider } from '@react-keycloak/web'
+import { SnackbarProvider } from 'notistack'
+import ReactDOM from 'react-dom/client'
+import { I18nextProvider } from 'react-i18next'
 
-import './i18n'
+import i18n from './i18n.ts'
+import { AppRouter } from './routes/AppRouter.tsx'
+import keycloak from './stores/AuthStore.ts'
+import apolloClient from './stores/GraphQlStore.ts'
 import './styles/css/index.css'
+import { theme } from './styles/ts/colors.ts'
 
-createRoot(document.querySelector('#root')!).render(
-    <StrictMode>
-        <p>Hello</p>
-    </StrictMode>
+ReactDOM.createRoot(document.querySelector('#root')!).render(
+  <ReactKeycloakProvider authClient={keycloak}>
+    <ApolloProvider client={apolloClient}>
+      <I18nextProvider i18n={i18n}>
+        <ThemeProvider theme={theme}>
+          <SnackbarProvider maxSnack={5}>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <AppRouter />
+            </LocalizationProvider>
+          </SnackbarProvider>
+        </ThemeProvider>
+      </I18nextProvider>
+    </ApolloProvider>
+  </ReactKeycloakProvider>
 )
